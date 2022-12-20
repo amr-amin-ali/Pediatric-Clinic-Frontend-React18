99fcb3af4a1f7dc1fbd8dc77285b775688b61c9f
Amr Amin <75953179+amr-amin-ali@users.noreply.github.com>
2022-12-20 21:28:46 +0200

    Minor bug fix

--- src/http/httpGET.js
+++ src/http/httpGET.js
@@ -14,5 +14,5 @@ export const httpGET = async (url = "") => {
     redirect: "follow", // manual, *follow, error
     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
   });
-  return response; // parses JSON response into native JavaScript objects
+  return response;
 };
